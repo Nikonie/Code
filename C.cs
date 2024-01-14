6396127b834b05be6f97d996633e24ade66c7397
Nikonie <2285645380@qq.com>
2024-01-14 16:19:08 +0800

    first commit

---
+++ C.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject dan1;
+    public GameObject dan2;
+
+
+    public void Activate1()
+    {
+        dan1.SetActive(true);
+    }
+    public void Activate2()
+    {
+        dan2.SetActive(true);
+    }
+    /// /
+    /// </summary>
+    public void Hide1()
+    {
+        dan1.SetActive(false);
+    }
+    public void Hide2()
+    {
+        dan2.SetActive(false);
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ main.py
@@ -0,0 +1,6 @@
+import  UnityEngine as ue
+
+objects =ue.Object.FindObjectsOfType(ue.GameObject)
+
+for go in objects:
+    ue.Debug.Log(go.name)
